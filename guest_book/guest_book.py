957d980238a5206dcf99bd394c20d9532a6d640a
asv-dev-tungpt <tungpt@aoi-sys.vn>
2015-01-16 18:38:24 +0700

    [Update01] Exercise 1: guestbook

--- guest_book/guest_book.py
+++ guest_book/guest_book.py
@@ -1,13 +1,14 @@
 __author__ = 'fatatoopc'
-
-import cgi
-import urllib
+from google.appengine.api import memcache
 from google.appengine.api import users
-
 from google.appengine.ext import ndb
-import webapp2
 import jinja2
 import os
+import cgi
+import cStringIO
+import logging
+import urllib
+import webapp2
 
 JINJA2_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -15,25 +16,7 @@ JINJA2_ENVIRONMENT = jinja2.Environment(
     autoescape = True
 )
 
-MAIN_PAGE_FOOTER_TEMPLATE = """\
-    <form action="/sign?%s" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-    <hr>
-    <form>Guestbook name:
-      <input value="%s" name="guestbook_name">
-      <input type="submit" value="switch">
-    </form>
-    <a href="%s">%s</a>
-  </body>
-</html>
-"""
-
-
 DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
-
-#We set a parent key
 
 def guestbook_key(guestbook_name = DEFAULT_GUESTBOOK_NAME):
     return ndb.Key('Guestbook', guestbook_name)
@@ -43,35 +26,54 @@ class Greeting(ndb.Model):
     content = ndb.StringProperty(indexed=False)
     date  = ndb.DateTimeProperty(auto_now_add=True)
 
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
-
+        self.response.out.write('<html><body>')
         guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
-        greeting_query = Greeting.query(
-            ancestor = guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greeting_query.fetch(10)
-
-
-
+        greetings = self.get_greetings(guestbook_name)
+        stats = memcache.get_stats()
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
-
         template_values = {
             'greetings': greetings,
             'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url' : url,
-            'url_linktext' : url_linktext,
+            'url': url,
+            'url_linktext': url_linktext,
+            'statshits': stats['hits'],
+            'statsmisses': stats['misses'],
         }
-
-
         template = JINJA2_ENVIRONMENT.get_template('index.html')
-
         self.response.write(template.render(template_values))
+
+    def get_greetings(self, guestbook_name):
+        greetings = memcache.get('%s:greetings' % guestbook_name)
+        if greetings is not None:
+            return greetings
+        else:
+            greetings = self.render_greetings(guestbook_name)
+            if not memcache.add('%s:greetings' % guestbook_name, greetings, 10):
+                logging.error('Memcache set failed.')
+        return greetings
+
+    def render_greetings(self, guestbook_name):
+        greetings_query = Greeting.query(
+            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(10)
+        output = cStringIO.StringIO()
+        for greeting in greetings:
+            if greeting.author:
+                self.response.write(
+                        '<b>%s</b> wrote:' % greeting.author.nickname())
+            else:
+                self.response.write('An anonymous person wrote:')
+            self.response.write('<blockquote>%s</blockquote>' %
+                                cgi.escape(greeting.content))
+        return output.getvalue()
+
 
 class Guestbook(webapp2.RequestHandler):
     def post(self):
@@ -85,7 +87,6 @@ class Guestbook(webapp2.RequestHandler):
 
         query_params = {'guestbook_name': guestbook_name}
         self.redirect('/?' + urllib.urlencode(query_params))
-
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
