8ddab44226da7b29382179811d463764eade9282
asv-dev-tungpt <tungpt@aoi-sys.vn>
2015-01-21 15:10:32 +0700

    [Update 5] Exercise 1-1

--- djangoguestbook/guestbook/views.py
+++ djangoguestbook/guestbook/views.py
@@ -2,19 +2,16 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from google.appengine.api import users
 from guestbook.models import Greeting, guestbook_key, DEFAULT_GUESTBOOK_NAME
-from django.views.decorators.cache import cache_page
+from google.appengine.api import memcache
 import urllib
+import logging
+import cStringIO
 
-@cache_page(10)
 def main_page(request):
     guestbook_name = request.GET.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
-    # Ancestor Queries, as shown here, are strongly consistent with the High
-    # Replication Datastore. Queries that span entity groups are eventually
-    # consistent. If we omitted the ancestor from this query there would be
-    # a slight chance that Greeting that had just been written would not
-    # show up in a query.
     greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-    greetings = greetings_query.fetch(10)
+    greetings = get_greetings(guestbook_name)
+    stats = memcache.get_stats()
     if users.get_current_user():
         url = users.create_logout_url(request.get_full_path())
         url_linktext = 'Logout'
@@ -26,6 +23,8 @@ def main_page(request):
         'guestbook_name': guestbook_name,
         'url': url,
         'url_linktext': url_linktext,
+        'statshits': stats['hits'],
+        'statsmisses': stats['misses'],
     }
     return render(request, 'guestbook/main_page.html', template_values)
 
@@ -39,3 +38,22 @@ def sign_post(request):
         greeting.put()
         return HttpResponseRedirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
     return HttpResponseRedirect('/')
+
+def get_greetings(guestbook_name):
+    greetings = memcache.get('%s:greetings' % guestbook_name)
+    if greetings is not None:
+        return greetings
+    else:
+        greetings = render_greetings(guestbook_name)
+        if not memcache.add('%s:greetings' % guestbook_name, greetings, 10):
+            logging.error('Memcache set failed.')
+    return greetings
+
+def render_greetings(guestbook_name):
+    logging.warning("done 1 !!!!!!!!!@#!@$#%^$&^*%$%^&*^%$#%^&%^$")
+    greetings_query = Greeting.query(
+        ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+    greetings = greetings_query.fetch(10)
+    output = cStringIO.StringIO()
+    logging.warning('Memcache set failed.')
+    return greetings
