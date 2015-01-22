6a662bbeabf12a9d37f5c4ffb2bf22b1aa06fd1e
asv-dev-tungpt <tungpt@aoi-sys.vn>
2015-01-22 11:12:22 +0700

    [Update 7] Exercise 1-1
    Chỉnh sửa lại memcache

--- djangoguestbook/guestbook/views.py
+++ djangoguestbook/guestbook/views.py
@@ -1,14 +1,13 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from google.appengine.api import users
-from guestbook.models import Greeting, guestbook_key, DEFAULT_GUESTBOOK_NAME
+from guestbook.models import Greeting, get_guestbook_key, DEFAULT_GUESTBOOK_NAME
 from google.appengine.api import memcache
 import urllib
 import logging
 
 def main_page(request):
     guestbook_name = request.GET.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
-    greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
     greetings = get_greetings(guestbook_name)
     stats = memcache.get_stats()
     if users.get_current_user():
@@ -30,11 +29,15 @@ def main_page(request):
 def sign_post(request):
     if request.method == 'POST':
         guestbook_name = request.POST.get('guestbook_name')
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
+        greeting = Greeting(parent=get_guestbook_key(guestbook_name))
         if users.get_current_user():
             greeting.author = users.get_current_user()
         greeting.content = request.POST.get('content')
         greeting.put()
+        #update to memcache
+        greetings = memcache.get('%s:greetings' % guestbook_name)
+        greetings.insert(0,greeting)
+        memcache.set('%s:greetings' % guestbook_name, greetings)
         return HttpResponseRedirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
     return HttpResponseRedirect('/')
 
@@ -43,13 +46,8 @@ def get_greetings(guestbook_name):
     if greetings is not None:
         return greetings
     else:
-        greetings = render_greetings(guestbook_name)
+        greetings_query = Greeting.query(ancestor=get_guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(10)
         if not memcache.add('%s:greetings' % guestbook_name, greetings, 10):
             logging.error('Memcache set failed.')
-    return greetings
-
-def render_greetings(guestbook_name):
-    greetings_query = Greeting.query(
-        ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-    greetings = greetings_query.fetch(10)
     return greetings
