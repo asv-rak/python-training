c470e42664afe77394069d4a38072cc5283994f0
asv-dev-tungpt <tungpt@aoi-sys.vn>
2015-01-22 10:40:22 +0700

    [Update02] Exercise 1
    Chỉnh sửa

--- guest_book/guest_book.py
+++ guest_book/guest_book.py
@@ -4,12 +4,10 @@ from google.appengine.api import users
 from google.appengine.ext import ndb
 import jinja2
 import os
-import cgi
-import cStringIO
 import logging
 import urllib
 import webapp2
-
+import datetime
 JINJA2_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
@@ -28,7 +26,6 @@ class Greeting(ndb.Model):
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('<html><body>')
         guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
         greetings = self.get_greetings(guestbook_name)
         stats = memcache.get_stats()
@@ -54,37 +51,26 @@ class MainPage(webapp2.RequestHandler):
         if greetings is not None:
             return greetings
         else:
-            greetings = self.render_greetings(guestbook_name)
-            if not memcache.add('%s:greetings' % guestbook_name, greetings, 10):
+            greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+            greetings = greetings_query.fetch(10)
+            if not memcache.set('%s:greetings' % guestbook_name, greetings, 10):
                 logging.error('Memcache set failed.')
         return greetings
 
-    def render_greetings(self, guestbook_name):
-        greetings_query = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
-        output = cStringIO.StringIO()
-        for greeting in greetings:
-            if greeting.author:
-                self.response.write(
-                        '<b>%s</b> wrote:' % greeting.author.nickname())
-            else:
-                self.response.write('An anonymous person wrote:')
-            self.response.write('<blockquote>%s</blockquote>' %
-                                cgi.escape(greeting.content))
-        return output.getvalue()
-
-
 class Guestbook(webapp2.RequestHandler):
     def post(self):
+        logging.warning("Go first!!!!!")
         guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
         greeting = Greeting(parent=guestbook_key(guestbook_name))
         if users.get_current_user():
             greeting.author = users.get_current_user()
-
         greeting.content = self.request.get('content')
         greeting.put()
-
+        #update to memcache
+        greetings = memcache.get('%s:greetings' % guestbook_name)
+        greetings.insert(0,greeting)
+        memcache.set('%s:greetings' % guestbook_name, greetings)
+        #redirect
         query_params = {'guestbook_name': guestbook_name}
         self.redirect('/?' + urllib.urlencode(query_params))
 
