43ee8ef1573264c4825b6c5f61ae4f8fa23218ed
asv-dev-tungpt <tungpt@aoi-sys.vn>
2015-01-30 13:39:39 +0700

    Edit-message

--- djangoguestbook/guestbook/urls.py
+++ djangoguestbook/guestbook/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from guestbook.views import GreetingView
+from guestbook.views import GreetingView, EditGreetingView
 
 urlpatterns = patterns(
 	'',
 	url(r'^$', GreetingView.as_view(), name='home'),
 	url(r'^sign/$', GreetingView.as_view(), name='sign'),
+	url(r'^edit/$', EditGreetingView.as_view(), name='edit'),
 )
--- djangoguestbook/guestbook/views.py
+++ djangoguestbook/guestbook/views.py
@@ -5,6 +5,46 @@ from guestbook.models import Greeting, DEFAULT_GUESTBOOK_NAME
 from guestbook.forms import PostForm
 
 
+class EditGreetingView(FormView):
+	template_name = "guestbook/main_page.html"
+	form_class = PostForm
+	edited_id = long(0)
+	old_guestbook_name = 'default_guestbook'
+
+	def get_context_data(self, **kwargs):
+		self.__class__.edited_id = long(self.request.GET.get('edited_message'))
+		self.__class__.old_guestbook_name = self.request.GET.get('old_guestbook_name')
+		guestbook_name = self.request.GET.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
+		greetings = Greeting.get_lastest(guestbook_name, 10, False)
+		if users.get_current_user():
+			url = users.create_logout_url(self.request.get_full_path())
+			url_linktext = 'Logout'
+		else:
+			url = users.create_login_url(self.request.get_full_path())
+			url_linktext = 'Login'
+		template_values = {
+			'greetings': greetings,
+			'guestbook_name': guestbook_name,
+			'url': url,
+			'url_linktext': url_linktext,
+			'form': kwargs['form'],
+			'edited_id': self.__class__.edited_id
+		}
+		context = template_values
+		return context
+
+	def form_valid(self, form):
+		guestbook_name = self.__class__.old_guestbook_name
+		dict = {
+			'guestbook_name': guestbook_name,
+			'update_by': users.get_current_user(),
+			'content': form.cleaned_data.get('content'),
+		}
+		Greeting.edit_greeting(int(self.__class__.edited_id), dict)
+		self.success_url = '/?' + urllib.urlencode({'guestbook_name': guestbook_name})
+		return super(EditGreetingView, self).form_valid(form)
+
+
 class GreetingView(FormView):
 	template_name = "guestbook/main_page.html"
 	force_new = False
@@ -48,8 +88,6 @@ class GreetingView(FormView):
 	@classmethod
 	def set_force_new(cls, _force_new):
 		cls.force_new = _force_new
-
-
 
 
 
