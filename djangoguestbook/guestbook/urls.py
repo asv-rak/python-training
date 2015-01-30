5281130c9d668d016ce3e4f5f68a76a3a5316137
asv-dev-tungpt <tungpt@aoi-sys.vn>
2015-01-30 10:43:15 +0700

    Submit file urls ( luc nay submit sot file)

--- djangoguestbook/guestbook/urls.py
+++ djangoguestbook/guestbook/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from guestbook.views import GreetingView
+from guestbook.views import GreetingView, DeleteGreetingView
 
 urlpatterns = patterns(
 	'',
 	url(r'^$', GreetingView.as_view(), name='home'),
 	url(r'^sign/$', GreetingView.as_view(), name='sign'),
+	url(r'^delete/$', DeleteGreetingView.as_view(), name='delete'),
 )
